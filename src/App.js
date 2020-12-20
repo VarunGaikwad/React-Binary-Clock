6639a2497b8839afb9734b7058617455973b6924
PreApeXis <gaikwadvarun23@gmail.com>
2020-12-20 22:57:06 +0530

    Code modified 🔥🎇🎆🏑

--- src/App.js
+++ src/App.js
@@ -16,37 +16,37 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App_Column  App__Hour__Tense">
+      <div className="App_Column ">
         <Fab color={convertNumberToBinary(time[0])[2] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[0])[3] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <label className="App__label">{time[0]}</label>
       </div>
-      <div className="App_Column App__Hour__Once">
+      <div className="App_Column">
         <Fab color={convertNumberToBinary(time[1])[1] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[1])[2] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[1])[3] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <label className="App__label">{time[1]}</label>
       </div>
-      <div className="App_Column App__Min__Tense">
+      <div className="App_Column">
         <Fab color={convertNumberToBinary(time[2])[1] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[2])[2] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[2])[3] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <label className="App__label">{time[2]}</label>
       </div>
-      <div className="App_Column App__Min__Once">
+      <div className="App_Column">
         <Fab color={convertNumberToBinary(time[3])[0] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[3])[1] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[3])[2] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[3])[3] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <label className="App__label">{time[3]}</label>
       </div>
-      <div className="App_Column App__Sec__Tense">
+      <div className="App_Column">
         <Fab color={convertNumberToBinary(time[4])[1] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[4])[2] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[4])[3] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <label className="App__label">{time[4]}</label>
       </div>
-      <div className="App_Column App__Sec__Once">
+      <div className="App_Column">
         <Fab color={convertNumberToBinary(time[5])[0] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[5])[1] === "1" ? "secondary" : "default"} size="medium"></Fab>
         <Fab color={convertNumberToBinary(time[5])[2] === "1" ? "secondary" : "default"} size="medium"></Fab>
